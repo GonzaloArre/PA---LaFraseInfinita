bdc829da73ae1a463c86fb8ab8cd8442aacc337e
Mariana0725 <marianasol0791@gmail.com>
2026-06-09 11:23:34 -0300

    Se agrega clase Usuario

---
+++ Biblio/People/Usuario.py
@@ -0,0 +1,16 @@
+from Biblio.Gente.Persona import Persona
+#Clase Usuario que representa a un usuario del sistema con credenciales de acceso. Hereda de la clase Persona.
+
+class Usuario(Persona):
+    def __init__(self, nombre, apellido, fec_nac, username, password):
+        super().__init__(nombre, apellido, fec_nac)
+        self.username = username #Nombre de usuario para el login
+        self.password = password #Contraseña para el login
+
+#Metodo para registrar el usuario en el sistema.
+    def agregar(self):
+        print(f"Usuario {self.username} agregado al sistema.")
+
+#Metodo str de Usuario para mostrar su informacion de manera legible.
+    def __str__(self):
+        return f"Usuario: {self.obtener_nombre_completo()} - Username: {self.username}"
