f91710c87f790419e5fc043d57aef1d377d96aab
Julian-uix <133913360+Julian-uix@users.noreply.github.com>
2026-06-27 15:54:07 -0300

    Update Usuario.py
    Modificación de Usuario para que un usuario del sistema solo tenga username y password, y así dejar Persona solo para Autor y Socio.

--- LaFraseInfinita/Biblio/People/Usuario.py
+++ LaFraseInfinita/Biblio/People/Usuario.py
@@ -1,16 +1,17 @@
-from Biblio.People.Persona import Persona
-#Clase Usuario que representa a un usuario del sistema con credenciales de acceso. Hereda de la clase Persona.
+class Usuario:
+    """
+    Representa un usuario del sistema con credenciales de acceso.
+    """
 
-class Usuario(Persona):
-    def __init__(self, nombre, apellido, fec_nac, username, password):
-        super().__init__(nombre, apellido, fec_nac)
-        self.username = username #Nombre de usuario para el login
-        self.password = password #Contraseña para el login
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-#Metodo para registrar el usuario en el sistema.
     def agregar(self):
-        print(f"Usuario {self.username} agregado al sistema.")
+        print(f"Usuario '{self.username}' agregado al sistema.")
 
-#Metodo str de Usuario para mostrar su informacion de manera legible.
+    def obtener_nombre_completo(self):
+        return self.username
+
     def __str__(self):
-        return f"Usuario: {self.obtener_nombre_completo()} - Username: {self.username}"
+        return f"Usuario: {self.username}"
